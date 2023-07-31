5cd00041d482189c6039d5f626ff35d4bf7c3156
[SW5] 조민호 <miinnnhho>
2023-07-31 17:39:16 +0900

    useRef 3

--- useref/src/App.js
+++ useref/src/App.js
@@ -2,32 +2,19 @@
 //그래서 var는 0으로 초기화 됐는데 Ref는 값을 표시해줌.
 
 import './App.css';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
-    const [renderer, setRenderer] = useState(0);
-    const countRef = useRef(0);
-    let countVar = 0;
+    const [count, setCount] = useState(1);
+    const renderCount = useRef(1);
 
-    const doRendering = () => {
-        setRenderer(renderer + 1);
-    };
-
-    const increseRef = () => {
-        countRef.current = countRef.current + 1;
-    };
-
-    const increseVar = () => {
-        countVar = countVar + 1;
-    };
-
+    useEffect(() => {
+        renderCount.current = renderCount.current + 1;
+    });
     return (
         <div>
-            <p>Ref: {countRef.current}</p>
-            <p>Var: {countVar}</p>
-            <button onClick={doRendering}>렌더링</button>
-            <button onClick={increseRef}>Ref 올려</button>
-            <button onClick={increseVar}>Var 올려</button>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>올려</button>
         </div>
     );
 }
