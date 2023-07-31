a9dcd261e9252fa8966373d989c1989d9edf3b5f
[SW5] 조민호 <miinnnhho>
2023-07-31 16:17:08 +0900

    useState 2

--- usestate/src/App.js
+++ usestate/src/App.js
@@ -1,23 +1,29 @@
 import './App.css';
 import { useState } from 'react';
 
-function App() {
-    const [time, setTime] = useState(1);
 
-    const handleClick = () => {
-        let newTime;
-        if (time >= 12) {
-            newTime = 1;
-        } else {
-            newTime = time + 1;
-        }
-        setTime(newTime);
+
+function App() {
+    const [names, setNames] = useState(['홍길동', '김민수']);
+    const [input, setInput] = useState('');
+
+    const handleInputChange = (e) => {
+        setInput(e.target.value);
+    };
+
+    const handleUpload = () => {
+        setNames((prevState) => {
+            return [input, ...prevState];
+        });
     };
 
     return (
         <div>
-            <span>now: {time}시</span>
-            <button onClick={handleClick}>update</button>
+            <input type="text" value={input} onChange={handleInputChange} />
+            <button onClick={handleUpload}>Upload</button>
+            {names.map((name, idx) => {
+                return <p key={idx}>{name}</p>;
+            })}
         </div>
     );
 }
